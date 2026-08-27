504dc71751469fe3a499a333116ae284e404228f
keeniGithub <maratkanovartem89@mail.ru>
2026-08-27 23:13:12 +0300

    add note and org/user name to dashboard title

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,4 @@
-import type { Metadata } from "next"
-
 import { MainLayoutClient } from "@/components/layouts/main-layout-client"
-
-export const metadata: Metadata = {
-  title: "Dashboard",
-}
 
 export default function MainLayout({
   children,
